79cde0b7d623d703655890378dfd3fdc925b76a3
Ahmed M. Elhalaby <eng.ahmed.elhalaby@gmail.com>
2021-01-10 15:36:34 +0200

    update

--- resources/views/Web/layouts/footer.blade.php
+++ resources/views/Web/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="container-fluid">
-    <div class=" mt-5 mb-4 footer_div w-100">
+    <div class=" mt-2 mb-1 footer_div w-100">
         <div class="row">
             <div class="col-lg-2 col-md-4 col-12 d-f">
                 <a href="" class="col-6 m-auto"><img class="footer-img" src="{{(app()->getLocale() == 'ar')?asset('web/img/footer.png'):asset('web/img/en_logo_white.png')}}" alt=""></a>
@@ -12,29 +12,15 @@
             <div class="col-lg-4 col-8 d-n">
                 <p class="footer-p">{{\App\Models\Setting::where('key','footer_about_us')->first()->value??''}}</p>
             </div>
-            <div class="col-lg-3 col-12 footer-email">
-                <p class="footer-p col-6"> <i class="fas fa-paper-plane ml-2"></i>{{__('web.User.email')}} : &nbsp; &nbsp;<span>{{\App\Models\Setting::where('key','email')->first()->value??'-'}}</span></p>
-                <p class="footer-p col-6"><i class="fas fa-phone ml-2"></i>{{__('web.User.mobile')}} :<span>&nbsp; &nbsp; {{\App\Models\Setting::where('key','mobile')->first()->value??'#'}}</span> </p>
-            </div>
-
-            <div class="col-lg-3 col-7 pl-0 our-app">
-                <p class="footer-p">{{__('web.download_our_app')}}</p>
-                <div class="flex social-btns">
-                    <a class="app-btn blu flex vert" href="http:apple.com">
-                        <p class="available">Available on the <br /> <span class="big-txt">App Store</span></p>
-                        <i class="fab fa-apple"></i>
-                    </a>
-                    <a class="app-btn blu flex vert" href="http:google.com">
-                        <p class="available">Get it on <br /> <span class="big-txt">Google Play</span></p>
-                        <i class="fab fa-google-play"></i>
-                    </a>
-                </div>
+            <div class="col-lg-6 col-12 footer-email">
+                <p class="footer-p col-lg-12 col-6"> <i class="fas fa-paper-plane ml-2"></i>{{__('web.User.email')}} : &nbsp; &nbsp;<span>{{\App\Models\Setting::where('key','email')->first()->value??'-'}}</span></p>
+                <p class="footer-p col-lg-12 col-6"><i class="fas fa-phone ml-2"></i>{{__('web.User.mobile')}} :<span>&nbsp; &nbsp; {{\App\Models\Setting::where('key','mobile')->first()->value??'#'}}</span> </p>
             </div>
         </div>
-        <hr>
+        <hr class="m-0">
         <div class="row">
             <div class="col-lg-6 col-12">
-                <ul class="sec-list">
+                <ul class="sec-list m-0">
                     <li><a href="{{url('terms')}}"> {{__('web.terms_and_conditions')}} </a></li>
                     <li><a href="{{url('privacy')}}"> {{__('web.privacy_and_conditions')}} </a></li>
                     <li><a href="{{url('commission')}}"> {{__('web.site_commission')}} </a></li>
@@ -42,7 +28,7 @@
                 </ul>
             </div>
             <div class="col-lg-6 col-12">
-                <p class="copyright">Copyright &copy; 2020 <a href="">Mnshour</a> </p>
+                <p class="copyright m-0">Copyright &copy; 2020 <a href="">Mnshour</a> </p>
             </div>
         </div>
     </div>
